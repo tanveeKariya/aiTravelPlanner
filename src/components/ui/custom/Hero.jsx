74d514d76a1b548d438871809e1aa6ec1398b654
Tanvee kariya <160867243+tanvee1909@users.noreply.github.com>
2024-09-29 13:15:32 +0530

    Initial commit

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -6,14 +6,15 @@ function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
         <h1 className='font-extrabold text-[50px] text-center mt-16'>
-            <span className='text-[#f56551]'>Discover Your Next Adventre with AI:</span> Personalized Interinaries</h1>
+            <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> Personalized Interinaries</h1>
 
         <p className='text-xl text-gray-500 text-center'>your personal trip planner is ready to provide you with a ease of tracking the whole trip within few seconds and tailored to your interest</p>
+        
         <Link to={'/create-trip'}> 
-        <Button> Get Started, It's Free</Button>
+          <Button className='cursor-pointer'> Get Started, It's Free</Button>
         </Link>
 
-        <img src='landing.png' className='-mt-20'/>
+        <img src='landing.png' className=''/>
     </div>
   )
 }
